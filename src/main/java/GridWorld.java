6770ee23400f236fc5a0ec8abdc7c72e6d750fe1
Noah Prince <noahprince8@gmail.com>
2015-11-28 19:23:22 -0600

    Allow terminal rewards

--- src/main/java/GridWorld.java
+++ src/main/java/GridWorld.java
@@ -19,23 +19,25 @@ public class GridWorld {
     }
 
     // Constructs the gridworld as defined in the mp
-    public GridWorld() {
+    public GridWorld(boolean rewardsTerminal) {
         grid = new GridSquare[6][6];
         for (int y = 0; y < 6; y++) {
             for (int x = 0; x < 6; x++) {
                 GridSquare square = new GridSquare(-0.04, false, false, x, y);;
                 if (x == 1 && y == 0) {
-                    square = new GridSquare(-1, false, false, x, y);
+                    square = new GridSquare(-1, rewardsTerminal, false, x, y);
                 } else if (x == 3 && ((y >= 1 && y <= 3) || y == 5)) { // y 1-3 and 5
-                    square = new GridSquare(0, false, true, x, y);
+                    square = new GridSquare(0, rewardsTerminal, true, x, y);
                 } else if (x == 0 && y == 5) {
-                    square = new GridSquare(1, false, false, x, y);
+                    square = new GridSquare(1, rewardsTerminal, false, x, y);
                 } else if (x == 1 && y == 5) {
-                    square = new GridSquare(-1, false, false, x, y);
+                    square = new GridSquare(-1, rewardsTerminal, false, x, y);
                 } else if (x == 4 && y == 1) {
-                    square = new GridSquare(-1, false, false, x, y);
+                    square = new GridSquare(-1, rewardsTerminal, false, x, y);
                 } else if ((x == 4 || x == 5) && y == 5) {
-                    square = new GridSquare(0, false, true, x, y);
+                    square = new GridSquare(0, rewardsTerminal, true, x, y);
+                } else if (x == 5 && y == 2) {
+                    square = new GridSquare(3, rewardsTerminal, true, x, y);
                 }
 
                 grid[y][x] = square;
@@ -46,7 +48,7 @@ public class GridWorld {
     }
 
     public static void main(String[] args) {
-        GridWorld world = new GridWorld();
+        GridWorld world = new GridWorld(false);
         DrawingBoard d = new DrawingBoard(world);
     }
 }
